a4ff9fe4f5483b60f892b1a5f5c09cb43fcb4ab0
Jurris Leuwe Tolosa <jleuwe@Power-Rangers.local>
2025-12-27 06:46:54 +0800

    fixed /login prerendering

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { getFirebaseAuth } from "@/lib/firebase/client";
